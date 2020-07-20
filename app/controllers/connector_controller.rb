9c0acb9f37ea1c887f1cec3bd470f7b9b9a283bb
debicky <arturd444@gmail.com>
2020-07-20 13:15:49 +0200

    serialize data from json

--- app/controllers/connector_controller.rb
+++ app/controllers/connector_controller.rb
@@ -2,6 +2,7 @@ class ConnectorController < ApplicationController
   def index
     api = HTTP.auth("Bearer #{ Rails.application.credentials.dig(:gist, :api_key) }").get("https://api.getgist.com/contacts")
     @response = JSON.parse(api)['contacts']
-    render :json => MultiJson.dump(@response.map { |i| i.compact })
+    render :json => MultiJson.dump(@response.map { |i| i.compact}, only: ["id", "first_name", "last_name", "full_name", "email"] )
   end
 end
+
